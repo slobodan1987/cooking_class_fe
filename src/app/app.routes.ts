207b83e7ad89d421ce37b52942f3e05be640b59c
slobodan1987 <slobodan.opsenica@gmail.com>
2025-08-01 18:35:42 +0200

    Attempt to stabilize redirections #1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './shared/home/home.component';
 
 export const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'index-hr.html', component: HomeComponent },
-  { path: '**', redirectTo: 'index-hr.html' },
+  { path: '**', redirectTo: '' },
 ];
