8ea4e7c3be62b42df230b8df4f2433e750a73f49
slobodan1987 <slobodan.opsenica@gmail.com>
2025-08-10 16:14:34 +0200

    chore: remove copy-static-to-dist script from build process

--- copy-static-to-dist.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if cp static.json dist/cooking_class_website/browser/; then
-    echo "✅ Copying of the static.json succeeded."
-else
-    echo "❌ Copying of the static.json failed."
-fi
